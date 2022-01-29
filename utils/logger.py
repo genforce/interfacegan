418d7ac65ff2a655a73cdc03ceff2aa928a96763
Clement Apavou <apavou.clement@outlook.fr>
2022-01-29 15:48:03 +0100

    comment errore

--- utils/logger.py
+++ utils/logger.py
@@ -35,10 +35,10 @@ def setup_logger(work_dir=None, logfile_name='log.txt', logger_name='logger'):
   """
 
   logger = logging.getLogger(logger_name)
-  if logger.hasHandlers():  # Already existed
-    raise SystemExit(f'Logger name `{logger_name}` has already been set up!\n'
-                     f'Please use another name, or otherwise the messages '
-                     f'may be mixed between these two loggers.')
+  # if logger.hasHandlers():  # Already existed
+  #   raise SystemExit(f'Logger name `{logger_name}` has already been set up!\n'
+  #                    f'Please use another name, or otherwise the messages '
+  #                    f'may be mixed between these two loggers.')
 
   logger.setLevel(logging.DEBUG)
   formatter = logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s")
